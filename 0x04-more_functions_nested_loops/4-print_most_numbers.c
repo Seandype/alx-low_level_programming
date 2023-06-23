2787b73c2a3ad7f23abccb2e1978050a425c3c0f
oluwaseun <seundipe2001@gmail.com>
2023-06-22 19:37:27 -0700

    numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  *print_most_numbers - prints the number 0 to 9
- *Description: Prints the numbers 0 to 9 excludingh 2 and 4
- *Return: The numbers 0 to 9 excluding 2 and 4
+ *Description: Prints the numbers 0 to 9 excluding 2 and 4
+ *Return: The numbers 0 to 9
  */
 
 void print_most_numbers(void)
@@ -21,5 +21,5 @@ void print_most_numbers(void)
 	_putchar(x + '0');
 	}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
