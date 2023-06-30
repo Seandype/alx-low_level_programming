238260effd0ed201d5e16eac9c1ce7f3c73feb97
oluwaseun <seundipe2001@gmail.com>
2023-06-29 21:33:41 -0700

    capitalize

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,11 +14,9 @@ char *string_toupper(char *s)
 
 	while (*(s + count) != '\0')
 	{
-	if ((*(s + count) >= 97) && (*(s + count) <= 122))
-	{
+	if ((*(s + count) >= 97) && (*(s + count) <= 122);
 	*(s + count) = *(s + count) - 32;
 	count++;
-	}
 	}
 
 	return (s);
